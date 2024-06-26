3481e300575fffaac444eca20608b78be1ee5b4f
bastienvanderchmitt <bastienvanderchmitt@gmail.com>
2024-06-26 21:09:59 +0200

    Design login page

--- src/components/pages/Admin/Login.js
+++ src/components/pages/Admin/Login.js
@@ -1,23 +1,54 @@
-import useApi from "../../../hooks/useApi";
 import { login } from "../../../api/user";
 import { authService } from "../../../helpers/authService";
 import toast, { Toaster } from "react-hot-toast";
 import { useAdminContext } from "../../../contexts/AdminContext";
-import { Button } from "reactstrap";
-import { Navigate, redirect } from "react-router-dom";
-import Dashboard from "./Dashboard";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Row,
+} from "reactstrap";
+import { Navigate } from "react-router-dom";
+import React, { useMemo } from "react";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import Field from "../../formik/Field";
+import logo from "../../../assets/img/logo_final.png";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import banner from "../../../assets/img/locations/location_11.jpeg";
 
 const Login = () => {
   const { setUser, setIsAuthenticated, isAuthenticated } = useAdminContext();
 
-  const logIn = async () => {
+  const initialValues = useMemo(() => {
+    return { username: "", password: "" };
+  }, []);
+
+  const validationSchema = useMemo(() => {
+    return Yup.object().shape({
+      username: Yup.string().required("Veuillez indiquer un login."),
+      password: Yup.string().required("Veuillez indiquer un mot de passe."),
+    });
+  }, []);
+
+  const handleForm = async (values) => {
     try {
-      const { data } = await login({ login: "admin", password: "admin" });
+      const { data } = await login({
+        login: values.username,
+        password: values.password,
+      });
       if (data?.user && data?.token) {
         data && authService.setUser(data);
         const user = authService.getUser();
         toast.success("Bienvenue " + user.name);
 
+        // TODO : UseFull ???
         setUser(user);
         setIsAuthenticated(true);
       } else {
@@ -32,10 +63,58 @@ const Login = () => {
   return isAuthenticated ? (
     <Navigate to="/admin/" replace />
   ) : (
-    <div>
+    <div
+      style={{
+        backgroundImage: "url(" + banner + ")",
+        backgroundSize: "cover",
+        height: "100vH",
+        width: "100vW",
+      }}
+    >
       <Toaster />
-      <h1>Login</h1>
-      <Button onClick={logIn}>LOGIN</Button>
+      <Container>
+        <Row className="justify-content-center" style={{ paddingTop: "15%" }}>
+          <Col lg={4} md={6} sm={12}>
+            <Card className="bg-transparent border-0">
+              <CardBody className="text-center bg-transparent p-4 text-white">
+                <img alt="logo" src={logo} style={{ marginTop: "-25px" }} />
+                <CardTitle tag="h4" className="mt-4">
+                  Connexion
+                </CardTitle>
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={handleForm}
+                  enableReinitialize
+                >
+                  {({ handleSubmit, isSubmitting }) => (
+                    <Form onSubmit={handleSubmit} className="w-100 mt-5">
+                      <FormGroup>
+                        <Field
+                          type="text"
+                          name="username"
+                          placeHolder="Login"
+                        />
+                      </FormGroup>
+                      <FormGroup>
+                        <Field
+                          type="password"
+                          name="password"
+                          placeHolder="Mot de passe"
+                        />
+                      </FormGroup>
+                      <Button color="primary text-white" block>
+                        <FontAwesomeIcon icon={faPaperPlane} className="me-2" />
+                        Se connecter
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
