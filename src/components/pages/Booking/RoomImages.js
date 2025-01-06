6efd1d3d039f882769cd6d9014d332761fb6832a
bastienvanderchmitt <bastienvanderchmitt@gmail.com>
2025-01-06 21:19:58 +0100

    Add pool images

--- src/components/pages/Booking/RoomImages.js
+++ src/components/pages/Booking/RoomImages.js
@@ -4,11 +4,11 @@ import { Container, Row } from "reactstrap";
 import { useCallback, useState } from "react";
 import room_1 from "../../../assets/img/room/room_1.jpeg";
 import room_2 from "../../../assets/img/room/room_2.jpeg";
-import room_3 from "../../../assets/img/room/room_3.jpeg";
+// import room_3 from "../../../assets/img/room/room_3.jpeg";
 import room_4 from "../../../assets/img/room/room_4.jpeg";
 import room_5 from "../../../assets/img/room/room_5.jpeg";
 import room_7 from "../../../assets/img/room/room_7.jpeg";
-import view_1 from "../../../assets/img/room/view_1.jpeg";
+import view_3 from "../../../assets/img/room/view_3.jpeg";
 import view_2 from "../../../assets/img/room/view_2.jpeg";
 
 const RoomImages = () => {
@@ -18,27 +18,27 @@ const RoomImages = () => {
   const images = [
     {
       src: room_2,
-      width: 3,
+      width: 4,
       height: 3,
     },
     {
       src: room_7,
-      width: 3,
+      width: 4,
       height: 3,
     },
     {
-      src: view_1,
+      src: view_3,
       width: 2,
       height: 1,
     },
-    {
-      src: room_3,
-      width: 3,
-      height: 4,
-    },
+    // {
+    //   src: room_3,
+    //   width: 3,
+    //   height: 4,
+    // },
     {
       src: view_2,
-      width: 4,
+      width: 5,
       height: 3,
     },
     {
